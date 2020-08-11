88cf222d3a133805691a401497b39de12348302a
darh0ky <jgamerino@gamil.com>
2020-08-11 16:49:50 -0700

    removed duplicate names

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,17 +5,10 @@ import '../styles/ContactForm.css'
 const ContactForm = () => (
   
 
- 
-<form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+  <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
 </form>
  
 
