91b65e87fc0c57f888f4f0a79150083d4f82f6bb
darh0ky <jgamerino@gamil.com>
2020-08-11 20:57:19 -0700

    d

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,25 +4,30 @@ import '../styles/ContactForm.css'
 
 const ContactForm = () => (
   
+  <div id="contact-form1" netlify className="bg-dark">
 
- <div id="contact_form1">
 <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
 </form>
- 
- </div>
+<input type="hidden" name="form-name" value="contact" />
+
+<form name="contact" netlify>
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+  </div>
 
 )
 
