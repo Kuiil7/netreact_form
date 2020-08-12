a6ae0f671d988bbcddc1328fc0a2c8f4042c0252
darh0ky <jgamerino@gamil.com>
2020-08-11 17:30:10 -0700

    added package-lock

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,7 +6,7 @@ const ContactForm = () => (
   
 
  <div id="contact_form1">
-<form name="contact" netlify data-netlify="true">
+<form name="contact" netlify netlify-honeypot="bot-field" hidden>
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
