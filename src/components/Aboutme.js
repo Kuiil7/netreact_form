83d149c874593ab71940c9c592137da1c7bbf5f6
kuiil7 <kuiil7ig11@gmail.com>
2020-09-16 00:33:12 -0700

    changing about me backg color

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -5,17 +5,17 @@ import "../styles/Aboutme.css";
 function Aboutme () {
   return (
 
-    <div id="about" className="bg-dark" >
-          <div id="about" className="container bg-dark">
+    <div id="about" >
+          <div id="about" className="container ">
 
     <center>
     <h2 id="aboutme_title">about me</h2>
-    <img src="https://i.imgrz.com/vb6h.jpg" id="prof-img" alt="profile pic"></img>
+    <img src="https://i.imgrz.com/vb6h.jpg" id="prof-img" className="p_margin20" alt="profile pic"></img>
 
     </center>
     <div>
-    <p>
-    In my experience, I always make things better. I started my career in Telecommunications working for MCI WorldCom and AT&T Relay Services in a national capacity. Through my career coding was always a necessity that I always filled in. As a self-taught coder, the George Washington University Bootcamp has provided me with all the necessary foundation, skill, and tools to create and build from scratch. I am passionate about creating a productive user experience and build apps that are accessible and are low-cost.   </p>
+    <p className="p_margin20" >
+    In my experience, I always make things better. I started my career in Telecommunications with MCI WorldCom and AT&T Relay Services in a national capacity. Through my career coding was always a necessity that I always filled in. As a self-taught coder, the George Washington University Bootcamp has provided me with all the necessary foundation, skill, and tools to create and build from scratch. I am passionate about creating a productive user experience and build apps that are accessible and are low-cost.   </p>
     <img src="https://i.imgrz.com/Yjwf.png" className="logo_size" alt="html5 logo"></img>
     <img src="https://i.imgrz.com/YAlk.png" className="logo_size" alt="CSS3 logo"></img>
     <img src="https://i.imgrz.com/YLb5.png" className="logo_size" alt="JavaScript logo"></img>
@@ -28,10 +28,10 @@ function Aboutme () {
     <img src=" https://i.imgrz.com/YpbV.png" className="logo_size" alt="Adobe Illustrator logo"></img>
     <img src="https://i.imgrz.com/Y27P.jpg" className="logo_size" alt="Photoshop logo"></img>
     <img src="  https://i.imgrz.com/RMw0.png" className="logo_size" alt="Final Cut Pro logo"></img>  
-<p>
+<p className="p_margin20" >
 Currently, I am focused on creating Progressive Web Apps and offline access. I am also very passionate the Android platform and ready for long-term commitment.
 </p>
-<p>
+<p className="p_margin20" >
 I bring: introducing new concepts and ideas | graphic design experience | technical expertise | a dedicated problem-solver | UX/UI design principles | Final Cut Pro 
 </p>
 </div>
