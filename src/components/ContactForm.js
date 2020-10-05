83b0c2ff13eb5f909f235382aa075deaf546f7dc
kuiil7 <kuiil7ig11@gmail.com>
2020-10-05 13:28:59 -0700

    removed navbar btn outline

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,40 +5,40 @@ import '../styles/ContactForm.css'
 const ContactForm = () => (
   
 
-<div id="contact1" class="bg-dark" >
+<div id="contact1" class="bg-dark">
 
-<form name="contact" className="contact-form" method="post" netlify >
-<p id="contact-me-title" className="text-center"  >
-      
-      CONTACT ME         
-            </p>
-          <input type="hidden" name="form-name" value="contact" />
+<form name="contact" className="contact-form" method="post" netlify>
+    <p id="contact-me-title" className="text-center">
 
-          <div className="form-field">
-      <label htmlFor="name">
-        <div className="label-content">Name:</div>
-        <input type="text" name="name" required />
-      </label>
+        CONTACT ME
+    </p>
+    <input type="hidden" name="form-name" value="contact" />
+
+    <div className="form-field">
+        <label htmlFor="name">
+    <div className="label-content">Name:</div>
+    <input type="text" name="name" required />
+  </label>
     </div>
 
     <div className="form-field">
-      <label htmlFor="email">
-        <div className="label-content">Email:</div>
-        <input type="email" name="email" required />
-      </label>
+        <label htmlFor="email">
+    <div className="label-content">Email:</div>
+    <input type="email" name="email" required />
+  </label>
     </div>
 
-          <div className="form-field">
-      <label htmlFor="message">
-        <div className="label-content">Message:</div>
-        <textarea className="stretch" name="message" rows="5" required />
-      </label>
+    <div className="form-field">
+        <label htmlFor="message">
+    <div className="label-content">Message:</div>
+    <textarea className="stretch" name="message" rows="5" required />
+  </label>
     </div>
-          
-          <button className="btn bg-warning" type="submit">Send</button>
-          
-        </form>
-  </div>
+
+    <button className="btn bg-warning" type="submit">Send</button>
+
+</form>
+</div>
 
 )
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,33 +6,34 @@ import '../styles/Navbar.css'
 function Navbar() {
   return (
 
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
 
     <a class="navbar-brand " href="home">JAM</a>
-    <button class="navbar-toggler btn .btn-outline-*  " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="nav-btn1 navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
 
-      <span class="navbar-toggler-icon  "></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      
-      <ul className="navbar-nav  mx-auto">
-        <li className="nav-item ">
-          <a className="nav-link" href="/#home">HOME </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/#about">ABOUT </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/#profile_main">PORTFOLIO</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/#contact1">CONTACT</a>
-        </li>
-     
- 
-      </ul>
+    
+</button>
+
+    <div className="collapse navbar-collapse"id="navbarSupportedContent">
+
+        <ul className="navbar-nav  mx-auto">
+            <li className="nav-item ">
+                <a className="nav-link" href="/#home">HOME </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#about">ABOUT </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#profile_main">PORTFOLIO</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#contact1">CONTACT</a>
+            </li>
+
+        </ul>
     </div>
-  </nav>
+</nav>
 
   );
 }
