371c3a19764cfa795612ae8a65083f4842061f49
kuiil7 <kuiil7ig11@gmail.com>
2020-12-01 10:32:28 -0800

    resizing landing page background image to 1200px

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,13 +10,13 @@ return (
        
  <div>
 
-<nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+<nav className="navbar fixed-top navbar-expand-md navbar-light bg-light">
   <a className="navbar-brand" href="/#section1">JGAM</a>
   <button className="navbar-toggler border-0 " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon "></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
+  <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
+    <div className="navbar-nav ml-auto">
       <a className="nav-item nav-link active" href="/#section1">Home <span className="sr-only">(current)</span></a>
       <a className="nav-item nav-link" href="/#section2">About Me</a>
       <a className="nav-item nav-link" href="/#section3">Portfolio</a>
